356aaab24cc58d3c35bc2d359b73b7cd9620cb3e
Mayra AlexandraTito Casavilca <mayrat.casavilca@gmail.com>
2019-02-15 13:03:12 -0500

    se agrego cambios en los templates agregando class

--- src/UI/templates/itemCommentPost.js
+++ src/UI/templates/itemCommentPost.js
@@ -1,12 +1,19 @@
 export default (idPost, dataPostComment, likesUsers, postCommentWall) => {
   const divContent = document.createElement('DIV');
   divContent.setAttribute('id', `idContainerComment_${idPost}`);
-  divContent.setAttribute('class', 'grid-item-comment');
+  divContent.setAttribute('class', 'grid-item-comment m-a p-5 b-r');
 
   const divContent_ = `
-  <input type="image" src="${dataPostComment.propietario.foto}" width="80" height="100" alt="Login"></input><br>
-  <label class = "slogan">${dataPostComment.propietario.nombre}</label><br>
-  <label class = "slogan">${dataPostComment.contenido}</label>
+ <div class="cont-float" style="overflow: none">
+  <div class="w-30p left">
+    <img src="${dataPostComment.propietario.foto}" class="avatar-perfil cursor ">
+  </div>
+  <div class="w-70p">
+    <label class="block left title">${dataPostComment.propietario.nombre}</label>
+    <br>
+    <p class="block ">${dataPostComment.contenido}</p>
+  </div>
+ </div>
     `;
   divContent.innerHTML = divContent_;
   postCommentWall.appendChild(divContent);
